6272ec9acb76594753dd02b2705e9d9347f232cf
Kemal Arifovic <gigalab@gmail.com>
2021-04-01 09:27:40 +0200

    Fetch - GET Async/Await

---
+++ ex02/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React, { Component } from "react";
+import GetRequestAsyncAwait from "./GetRequestAsyncAwait";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GetRequestAsyncAwait />
+      </div>
+    );
+  }
+}
+
+export default App;
